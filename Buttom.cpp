9075ae2ec24ef8a5964429d16104bd743b054fce
AlexGLT <oles.glts@gmail.com>
2019-03-03 15:53:41 +0200

    Revert

--- Buttom.cpp
+++
@@ -1,6 +0,0 @@
-#include "Buttom.h"
-#include "Renderer.h"
-void Buttom::draft(Renderer &a) {
-	a.set_color(red, green, blue);
-	a.fill_rect(x, y, weight, height);
-}
--- Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Application.h"
-#include <iostream>
-
-int main(int argc, char* args[])
-{
-	Application application;
-	application.execute();
-	return 0;
-}
